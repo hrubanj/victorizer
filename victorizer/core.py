9885de15644cdfe505a1cc89765488dba4b9b4b5
Samuel Hudec <39057205+SamuelHudec@users.noreply.github.com>
2022-05-31 17:21:15 +0200

    Update core.py
    add new short cut method

--- victorizer/core.py
+++ victorizer/core.py
@@ -4,6 +4,7 @@ import random
 
 WISDOMS_FILENAME = "wisdoms.txt"
 PAPER_LINKS_FILENAME = "paper_links.txt"
+SHORT_CUT_FILENAME = "short_cuts.txt"
 RESOURCES_PACKAGE_NAME = "victorizer.resources"
 
 
@@ -22,3 +23,6 @@ def get_wisdom() -> str:
 
 def get_paper_link() -> str:
     return select_random_line(PAPER_LINKS_FILENAME)
+
+def get_short_cut() -> str:
+    return select_random_line(SHORT_CUT_FILENAME)
